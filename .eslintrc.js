c64b05a682470d9d127c75bad0d761077f8a9a54
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2021-11-20 15:39:40 +0800

    feat:暂时关闭prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: wuchen
+ * @Date: 2021-11-16 16:18:38
+ * @LastEditors: wuchen
+ * @LastEditTime: 2021-11-20 15:30:26
+ * @Description: 
+ * @Email: rangowu@163.com
+ */
 module.exports = {
   root: true,
   env: {
@@ -7,8 +15,8 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    // "@vue/prettier",
+    // "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,5 +24,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    //关闭prettier
+    "prettier/prettier": "off"
   },
 };
