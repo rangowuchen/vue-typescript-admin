c972244064070e08888f22b7ecdc49fa130b4f26
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2021-11-22 16:12:21 +0800

    feat:添加eslint规范,卸载prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,12 @@
  * @Author: wuchen
  * @Date: 2021-11-16 16:18:38
  * @LastEditors: wuchen
- * @LastEditTime: 2021-11-20 15:30:26
- * @Description: 
+ * @LastEditTime: 2021-11-22 16:07:26
+ * @Description: eslint规则:https://eslint.bootcss.com/docs/rules/
+ * 1,配置规则
+ * off:关闭规则
+ * warn:开启规则,使用警告级别的错误,不会导致程序退出
+ * error:开启规则,使用错误级别的错误,当被触发的时候，程序会退出
  * @Email: rangowu@163.com
  */
 module.exports = {
@@ -14,17 +18,67 @@ module.exports = {
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
-    // "@vue/prettier",
-    // "@vue/prettier/@typescript-eslint",
+    "@vue/typescript/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    //关闭prettier
-    "prettier/prettier": "off"
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error',
+      {
+        multiline: {
+          delimiter: 'none'
+        },
+        singleline: {
+          delimiter: 'comma'
+        }
+      }],
+      // 可以使用any
+    '@typescript-eslint/no-explicit-any': 'off',
+    // 可以使用var
+    '@typescript-eslint/no-var-requires': 'off',
+    // 生产环境禁用 console
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 生产环境禁用 debugger
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 强制在 function的左括号之前使用一致的空格
+    'space-before-function-paren': ['error', 'never'],
+    // 强制数组方括号中使用一致的空格
+    'vue/array-bracket-spacing': 'error',
+    // 强制箭头函数的箭头前后使用一致的空格
+    'vue/arrow-spacing': 'error',
+    // 强制在单行代码块中使用一致的空格
+    'vue/block-spacing': 'error',
+    // 强制在代码块中使用一致的大括号风格
+    'vue/brace-style': 'error',
+    // 强制使用骆驼拼写法命名约定
+    'vue/camelcase': 'error',
+    // 要求或禁止末尾逗号
+    'vue/comma-dangle': 'error',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    // 要求使用 === 和 !==
+    'vue/eqeqeq': 'error',
+    // 强制在对象字面量的属性中键和值之间使用一致的间距
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    // 强制在大括号中使用一致的空格
+    'vue/object-curly-spacing': 'error'
   },
+  // 为特定类型的文件指定处理器
+  overrides: [
+    {
+      // 指定文件
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      // 指定环境
+      env: {
+        // Jest 全局变量
+        jest: true
+      }
+    }
+  ]
 };
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,10 +2,144 @@
  * @Author: wuchen
  * @Date: 2021-11-20 14:16:05
  * @LastEditors: wuchen
- * @LastEditTime: 2021-11-20 14:16:06
+ * @LastEditTime: 2021-11-22 14:18:30
  * @Description: 
  * @Email: rangowu@163.com
 -->
 <template>
-    <div>登录</div>
+    <div class="login-container">
+        <el-form
+        ref="loginForm"
+        :model="loginForm"
+        :rules="loginRules"
+        class="login-form"
+        autocomplete="on"
+        label-position="left"
+        >
+        <div class="title-container">
+            <h3 class="title">
+            {{ $t('login.title') }}
+            </h3>
+            <lang-select class="set-language" />
+        </div>
+
+        <el-form-item prop="username">
+            <span class="svg-container">
+            <svg-icon name="user" />
+            </span>
+            <el-input
+            ref="username"
+            v-model="loginForm.username"
+            :placeholder="$t('login.username')"
+            name="username"
+            type="text"
+            tabindex="1"
+            autocomplete="on"
+            />
+        </el-form-item>
+
+        <el-tooltip
+            v-model="capsTooltip"
+            content="Caps lock is On"
+            placement="right"
+            manual
+        >
+            <el-form-item prop="password">
+            <span class="svg-container">
+                <svg-icon name="password" />
+            </span>
+            <el-input
+                :key="passwordType"
+                ref="password"
+                v-model="loginForm.password"
+                :type="passwordType"
+                :placeholder="$t('login.password')"
+                name="password"
+                tabindex="2"
+                autocomplete="on"
+                @keyup.native="checkCapslock"
+                @blur="capsTooltip = false"
+                @keyup.enter.native="handleLogin"
+            />
+            <span
+                class="show-pwd"
+                @click="showPwd"
+            >
+                <svg-icon :name="passwordType === 'password' ? 'eye-off' : 'eye-on'" />
+            </span>
+            </el-form-item>
+        </el-tooltip>
+
+        <el-button
+            :loading="loading"
+            type="primary"
+            style="width:100%; margin-bottom:30px;"
+            @click.native.prevent="handleLogin"
+        >
+            {{ $t('login.logIn') }}
+        </el-button>
+
+        <div style="position:relative">
+
+            <el-button
+            class="thirdparty-button"
+            type="primary"
+            @click="showDialog=true"
+            >
+            {{ $t('login.thirdparty') }}
+            </el-button>
+        </div>
+        </el-form>
+
+        <el-dialog
+        :title="$t('login.thirdparty')"
+        :visible.sync="showDialog"
+        >
+        {{ $t('login.thirdpartyTips') }}
+        <br>
+        <br>
+        <br>
+        <social-sign />
+        </el-dialog>
+    </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+// import { Route } from 'vue-router';
+
+@Component({
+    name: 'Login',
+    components: {
+        
+    }
+})
+export default class extends Vue {
+    // 表单验证
+    private validateUsername = (rule: any, value: string, callback: Function) => {
+    if (!value) {
+      callback(new Error('Please enter the correct user name'))
+    } else {
+      callback()
+    }
+  }
+
+  private validatePassword = (rule: any, value: string, callback: Function) => {
+    if (value.length < 6) {
+      callback(new Error('The password can not be less than 6 digits'))
+    } else {
+      callback()
+    }
+  }
+
+    private loginForm = {
+        username: 'admin',
+        password: '111111'
+    }
+
+    private loginRules = {
+        username: [{ validator: this.validateUsername, trigger: 'blur'}],
+        password: [{ validator: this.validatePassword, trigger: 'blur'}]
+    }
+}
+</script>
